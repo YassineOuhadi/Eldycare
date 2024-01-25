22ef23618df11103c3a48499baf9a3f467604046
YaasineOuhadi <yassinouhadi@gmail.com>
2024-01-25 14:19:44 +0100

    change main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -5,14 +5,23 @@ provider "aws" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eldycare-cluster"
-  vpc_id          = "vpc-04e1603798ea0e88d"
   cluster_version = "1.21"
+
+  vpc_id       = "vpc-04e1603798ea0e88d"
+  subnets      = ["subnet-0af154e1ea5025a6b", "subnet-0cc88ba02fb8a36a7", "subnet-0374d3ab0d4715e9e", "subnet-0e04f8052bf1e758a"]
+
   node_groups = {
     eks_nodes = {
-      desired_capacity = 2
+      desired_capacity = 3
       max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t2.micro"
+      min_capaicty     = 3
+       instance_type    = "t2.micro"
+
+      instance_type = "t2.small"
     }
   }
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
 }
