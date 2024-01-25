a893ee80cabff8477ab36bb1cc6311c8551a61af
YaasineOuhadi <yassinouhadi@gmail.com>
2024-01-25 15:03:03 +0100

    change main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -8,18 +8,6 @@ module "eks" {
   cluster_version = "1.21"
 
   vpc_id       = "vpc-04e1603798ea0e88d"
-  subnets      = "subnet-0374d3ab0d4715e9e"
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 3
-      max_capacity     = 3
-      min_capaicty     = 3
-       instance_type    = "t2.micro"
-
-      instance_type = "t2.small"
-    }
-  }
 }
 
 output "kubeconfig" {
